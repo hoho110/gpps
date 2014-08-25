8e126670bc7cb40b1e924dd6bd6ac1b42b4a92d7
wmsdu <wmsdu@aliyun.com>
2014-08-25 17:03:34 +0800

    提交payback的model

--- src/main/java/gpps/model/Payback.java
+++ src/main/java/gpps/model/Payback.java
@@ -18,4 +18,46 @@ public class Payback {
 //	 * 0：正常还款 1：新增（指向延期）
 //	 */
 //	private int type;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getBorrowerAccountId() {
+		return borrowerAccountId;
+	}
+	public void setBorrowerAccountId(Integer borrowerAccountId) {
+		this.borrowerAccountId = borrowerAccountId;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public BigDecimal getChiefAmount() {
+		return chiefAmount;
+	}
+	public void setChiefAmount(BigDecimal chiefAmount) {
+		this.chiefAmount = chiefAmount;
+	}
+	public BigDecimal getInterest() {
+		return interest;
+	}
+	public void setInterest(BigDecimal interest) {
+		this.interest = interest;
+	}
+	public long getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(long deadline) {
+		this.deadline = deadline;
+	}
 }

4f7b6b5581228bedad2dd0daae260814c434d856
wmsdu <wmsdu@aliyun.com>
2014-08-25 16:31:39 +0800

    修改payback

--- src/main/java/gpps/dao/IPayBackDao.java
+++ src/main/java/gpps/dao/IPayBackDao.java
@@ -9,14 +9,14 @@ import org.apache.ibatis.annotations.Param;
 
 import gpps.model.Payback;
 
-/**
- * @author wangm
- *
- */
 public interface IPayBackDao {
 	public void create(Payback payback);
+
 	public List<Payback> findAllByProduct(Integer productId);
-	public void changeState(@Param("id")Integer id,@Param("state")int state);
+
+	public void changeState(@Param("id") Integer id, @Param("state") int state);
+
 	public Payback find(Integer id);
+
 	public void delete(Integer id);
 }

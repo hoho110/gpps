9443871cdfd5ef751843b28e0dc7cb5fb7fbd486
wd <wd@wd-PC>
2014-09-27 01:33:36 +0800

    登陆,注册,密码找回一套功能的实现

---
+++ src/main/webapp/WEB-INF/views/google/js/foot.js
@@ -0,0 +1,35 @@
+foot();
+function foot(){
+	var str='<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="border-bottom : #999 dotted 1px; padding : 10px 10px 10px 10px;">';
+	str += '<strong>友情链接:</strong><a href="#">网贷天眼</a><a href="#">网贷之家</a><a href="#">融投贷</a><a href="#">陆金所</a><a href="#">人人贷</a>';
+	str += '</div>';
+	str += '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding : 10px 10px 10px 10px;">';
+	str += '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="text-align:center; color:#999">';
+	str += '政采贷  Copyright © 2013-2014. All rights reserved<br>';
+	str += '地址：北京XXXXXXXXXXX国际大厦<br>';
+	str += '京ICP备14001357号-3';
+	str += '</div>';
+	str += '<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">';
+	str += '<img src="img/cx.jpg"></img>';
+	str += '<img src="img/hftxlogo.jpg"></img>';
+	str += '<img src="img/wxrz.jpg"></img>';
+	str += '</div></div>';
+	
+	$('div#footer').html(str);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/webapp/WEB-INF/views/google/js/head.js
@@ -0,0 +1,39 @@
+var service = EasyServiceClient.getRemoteProxy("/easyservice/gpps.service.ILenderService");
+var cuser = service.getCurrentUser();
+function header(title){
+	var divtitle = $('<div style="display:inline;"></div>');
+	divtitle.append('<h2 style="float:left;" class="text-muted">政采贷<small>&nbsp;&nbsp;信用创造价值</small></h2>');
+	var divusercontent = $('<div id="usercontent" style="float:right; font-size:16px; padding-top:30px;"></div>');
+	
+	if(title=='login' || title=='register'){
+		if(cuser!=null)
+		{
+			window.location.href="myaccount.html";
+		}else
+			{
+		divusercontent.html('');
+			}
+	}else{
+	
+	if(cuser==null){
+		divusercontent.html('<a href="login.html">登陆</a><span>&nbsp;|&nbsp;</span><a href="register.html">注册</a>');
+	}else{
+		divusercontent.html('下午好：'+cuser.loginId+'&nbsp;&nbsp;|&nbsp;&nbsp;<a href="myaccount.html">我的账户</a>'+'&nbsp;&nbsp;|&nbsp;&nbsp;<a href="quit.html">退出</a>');
+	}
+		}
+	divtitle.append(divusercontent);
+	divtitle.append('<div style="clear:both;"></div>');
+	
+	var navul = $('<ul class="nav nav-justified"></ul>');
+	navul.append('<li id="index"><a class="active" href="index.html">首页</a></li>');
+	navul.append('<li id="product"><a href="productlist.html">我要理财</a></li>');
+	navul.append('<li id="myaccount"><a href="myaccount.html">我的帐户</a></li>');
+	navul.append('<li id="activity"><a href="activity.html">活动中心</a></li>');
+	navul.append('<li id="loan"><a href="loan.html">我要融资</a></li>');
+	
+	$('div#header').html('');
+	$('#header').append(divtitle);
+	$('#header').append(navul);
+	
+	$('li[id="'+title+'"]').addClass('active');
+}

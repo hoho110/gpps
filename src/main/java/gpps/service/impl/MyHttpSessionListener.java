bc9aef8a98d8dbc3fdcc26796f0768d41f239646
wmsdu <wmsdu@aliyun.com>
2014-10-09 16:10:29 +0800

    增加对Session的监听

---
+++ src/main/java/gpps/service/impl/MyHttpSessionListener.java
@@ -0,0 +1,18 @@
+package gpps.service.impl;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.springframework.stereotype.Service;
+@Service
+public class MyHttpSessionListener implements HttpSessionListener{
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		System.out.println("创建一个Session");
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		System.out.println("销毁一个Session");
+	}
+}

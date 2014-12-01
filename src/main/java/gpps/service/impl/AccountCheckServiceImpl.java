7a1c9c5a8f6775f658c2ab6e136db59ff89c3b76
wangm <wangm@wangm-Founder-PC>
2014-12-01 16:27:16 +0800

    为校验加上trycatch

--- src/main/java/gpps/service/impl/AccountCheckServiceImpl.java
+++ src/main/java/gpps/service/impl/AccountCheckServiceImpl.java
@@ -1,6 +1,5 @@
 package gpps.service.impl;
 
-import gpps.constant.Pagination;
 import gpps.dao.IBorrowerAccountDao;
 import gpps.dao.ICashStreamDao;
 import gpps.dao.ILenderAccountDao;
@@ -22,6 +21,7 @@ import java.util.concurrent.FutureTask;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,6 +40,7 @@ public class AccountCheckServiceImpl implements IAccountCheckService {
 	private ILenderDao lenderDao;
 	private static final int DEFAULT_RECNUM=100;
 	private static final String NEWLINE="\r\n";
+	Logger logger=Logger.getLogger(AccountCheckServiceImpl.class);
 	@PostConstruct
 	public void init() {
 		checkAccount();
@@ -52,70 +53,77 @@ public class AccountCheckServiceImpl implements IAccountCheckService {
 			@Override
 			public String call() throws Exception {
 				StringBuilder sBuilder=new StringBuilder();
-				int total=lenderDao.countAll();
-				for(int i=0;i<(total/DEFAULT_RECNUM+(total%DEFAULT_RECNUM==0?0:1));i++)
+				try
 				{
-					List<Lender> lenders=lenderDao.findAll(i*DEFAULT_RECNUM, DEFAULT_RECNUM);
-					if(lenders==null||lenders.size()==0)
-						continue;
-					for(Lender lender:lenders)
+					int total=lenderDao.countAll();
+					for(int i=0;i<(total/DEFAULT_RECNUM+(total%DEFAULT_RECNUM==0?0:1));i++)
 					{
-						if(StringUtil.isEmpty(lender.getThirdPartyAccount()))
+						List<Lender> lenders=lenderDao.findAll(i*DEFAULT_RECNUM, DEFAULT_RECNUM);
+						if(lenders==null||lenders.size()==0)
 							continue;
-						//与第三方验证
-						//网贷平台子账户可用余额|总可用余额(子账户可用余额+公共账户可用余额)|子账户冻结余额”（例:100.00|200.00|10.00）
-						String text=thirdPaySupportService.balanceQuery(lender.getThirdPartyAccount());
-						if(StringUtil.isEmpty(text))
+						for(Lender lender:lenders)
 						{
-							appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "从第三方支付平台获取账户信息失败.");
-							continue;
-						}
-						LenderAccount account=lenderAccountDao.find(lender.getAccountId());
-						String[] thirdAccount=text.split("\\|");
-						if(!compareAccount(thirdAccount[0], account.getUsable())||!compareAccount(thirdAccount[2], account.getFreeze()))
-						{
-							appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), 
-									"本地账户与第三方支付平台不符,本地可用|冻结金额为"+account.getUsable().toString()+"|"+account.getFreeze().toString()+";"
-									+"第三方可用|冻结金额为"+thirdAccount[0]+"|"+thirdAccount[2]);
-						}
-						//验证：总金额=可用金额+冻结金额+已投资金额
-						if(account.getFreeze().add(account.getUsable()).add(account.getUsed()).compareTo(account.getTotal())!=0)
-						{
-							appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "账户金额错误,总金额不等于可用金额+冻结金额+已投资金额");
-						}
-						//可用金额=所有现金流之和
-						CashStreamSum sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, null);
-						if(account.getUsable().compareTo(sum.getChiefAmount().add(sum.getInterest()))!=0)
-						{
-							appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "可用金额与现金流验证错误,可用金额:"+account.getUsable().toString()+",现金流:"+sum);
-						}
-						//冻结金额=冻结+解冻
-						List<Integer> actions=new ArrayList<Integer>();
-						actions.add(CashStream.ACTION_FREEZE);
-						actions.add(CashStream.ACTION_UNFREEZE);
-						sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, actions);
-						if(account.getFreeze().compareTo(sum.getChiefAmount().add(sum.getInterest()))!=0)
-						{
-							appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "冻结金额与现金流验证错误,冻结金额:"+account.getFreeze().toString()+",现金流:"+sum);
-						}
-						//已投资金额=购买+回款（本金）
-						actions=new ArrayList<Integer>();
-						actions.add(CashStream.ACTION_PAY);
-						actions.add(CashStream.ACTION_REPAY);
-						sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, actions);
-						if(account.getUsed().negate().compareTo(sum.getChiefAmount())!=0)
-						{
-							appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "已用金额与现金流验证错误,已用金额:"+account.getFreeze().toString()+",现金流:"+sum.getChiefAmount());
-						}
-						//利息=回款（利息）
-						actions=new ArrayList<Integer>();
-						actions.add(CashStream.ACTION_REPAY);
-						sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, actions);
-						if(account.getTotalincome().compareTo(sum.getInterest())!=0)
-						{
-							appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "已收益金额与现金流验证错误,已收益金额:"+account.getTotalincome().toString()+",现金流:"+sum.getInterest());
+							if(StringUtil.isEmpty(lender.getThirdPartyAccount()))
+								continue;
+							//与第三方验证
+							//网贷平台子账户可用余额|总可用余额(子账户可用余额+公共账户可用余额)|子账户冻结余额”（例:100.00|200.00|10.00）
+							String text=thirdPaySupportService.balanceQuery(lender.getThirdPartyAccount());
+							if(StringUtil.isEmpty(text))
+							{
+								appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "从第三方支付平台获取账户信息失败.");
+								continue;
+							}
+							LenderAccount account=lenderAccountDao.find(lender.getAccountId());
+							String[] thirdAccount=text.split("\\|");
+							if(!compareAccount(thirdAccount[0], account.getUsable())||!compareAccount(thirdAccount[2], account.getFreeze()))
+							{
+								appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), 
+										"本地账户与第三方支付平台不符,本地可用|冻结金额为"+account.getUsable().toString()+"|"+account.getFreeze().toString()+";"
+										+"第三方可用|冻结金额为"+thirdAccount[0]+"|"+thirdAccount[2]);
+							}
+							//验证：总金额=可用金额+冻结金额+已投资金额
+							if(account.getFreeze().add(account.getUsable()).add(account.getUsed()).compareTo(account.getTotal())!=0)
+							{
+								appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "账户金额错误,总金额不等于可用金额+冻结金额+已投资金额");
+							}
+							//可用金额=所有现金流之和
+							CashStreamSum sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, null);
+							if(account.getUsable().compareTo(sum.getChiefAmount().add(sum.getInterest()))!=0)
+							{
+								appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "可用金额与现金流验证错误,可用金额:"+account.getUsable().toString()+",现金流:"+sum);
+							}
+							//冻结金额=冻结+解冻
+							List<Integer> actions=new ArrayList<Integer>();
+							actions.add(CashStream.ACTION_FREEZE);
+							actions.add(CashStream.ACTION_UNFREEZE);
+							sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, actions);
+							if(account.getFreeze().compareTo(sum.getChiefAmount().add(sum.getInterest()))!=0)
+							{
+								appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "冻结金额与现金流验证错误,冻结金额:"+account.getFreeze().toString()+",现金流:"+sum);
+							}
+							//已投资金额=购买+回款（本金）
+							actions=new ArrayList<Integer>();
+							actions.add(CashStream.ACTION_PAY);
+							actions.add(CashStream.ACTION_REPAY);
+							sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, actions);
+							if(account.getUsed().negate().compareTo(sum.getChiefAmount())!=0)
+							{
+								appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "已用金额与现金流验证错误,已用金额:"+account.getFreeze().toString()+",现金流:"+sum.getChiefAmount());
+							}
+							//利息=回款（利息）
+							actions=new ArrayList<Integer>();
+							actions.add(CashStream.ACTION_REPAY);
+							sum=cashStreamDao.sumCashStream(lender.getAccountId(), null, actions);
+							if(account.getTotalincome().compareTo(sum.getInterest())!=0)
+							{
+								appendMsg(sBuilder, Lender.class, lender.getId(), lender.getThirdPartyAccount(), "已收益金额与现金流验证错误,已收益金额:"+account.getTotalincome().toString()+",现金流:"+sum.getInterest());
+							}
 						}
 					}
+				}catch(Throwable e)
+				{
+					logger.error(e.getMessage(),e);
+					sBuilder.append(e.getMessage());
 				}
 				//验证borrower
 				return sBuilder.toString();

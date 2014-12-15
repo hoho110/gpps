6efb2d1223343a5fce91cd0c42ed89c99a0489ba
calis <calis@calis-PC>
2014-12-15 15:32:47 +0800

    添加余额不足类信息

--- src/main/java/gpps/service/exception/InsufficientBalanceException.java
+++ src/main/java/gpps/service/exception/InsufficientBalanceException.java
@@ -3,7 +3,7 @@ package gpps.service.exception;
 public class InsufficientBalanceException extends Exception{
 	public InsufficientBalanceException()
 	{
-		super();
+		super("余额不足");
 	}
 	public InsufficientBalanceException(String msg)
 	{

a2a8720d1f47806aa6c0b9ee3ff17012b928ff25
calis <calis@calis-PC>
2014-12-30 15:58:12 +0800

    增加短信服务

---
+++ src/main/java/gpps/service/exception/SMSException.java
@@ -0,0 +1,10 @@
+package gpps.service.exception;
+
+public class SMSException extends Exception{
+	public SMSException(){
+		super();
+	}
+	public SMSException(String message){
+		super(message);
+	}
+}
